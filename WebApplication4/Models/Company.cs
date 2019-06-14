b1f8a2f86d807bdee1d4c35a90c1ffb44e1d7eed
Daunte Dottery <d.dottery4@gmail.com>
2019-06-14 10:16:10 -0400

    Add project files.

---
+++ WebApplication4/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Company
+    {
+        public int CompanyID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
